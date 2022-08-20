e5e3d976fbf368c5a46253a7df3aeed865a572b7
Rafaela Siqueira <54718780+Rafaela1993@users.noreply.github.com>
2022-08-20 18:19:48 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -9,7 +9,7 @@ apt-get install unzip -y
 echo "Baixando e copiando arquivos da aplicação ..."
 
 cd /tmp
-wget https://github.com/Rafaela1993/linux-projeto1-iac/archive/refs/heads/main.zip
+wget 
 unzip main.zip
 cd linux-projeto-iac-main
 cp -R * /var/www/html/
